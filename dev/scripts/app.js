afce760375203aa7bdf6358b9b607fc4bed9ad20
Brett Nielsen <rebel.intents@gmail.com>
2017-12-06 12:30:31 -0500

    Completed Ajax call to API

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -5,13 +5,49 @@ import {
 	Route, Link } from 'react-router-dom';
 import $ from 'jquery';
 
+const key = "e702b77b348838b660167fadf559e91c";
+
 class App extends React.Component {
+	constructor(props){
+		super();
+		this.state = {
+			city: ""
+		}
+		this.changeCity = this.changeCity.bind(this);
+		this.getWeather = this.getWeather.bind(this);
+	};
+	getWeather(e){
+		e.preventDefault();
+		$.ajax({
+			url: "http://api.openweathermap.org/data/2.5/forecast?mode=json",
+			data: {
+				q: this.state.city,
+				APPID: key,
+				units: "metric"
+			}
+		}).then((res) => {
+			console.log(res);
+		});
+	};
+	changeCity(e){
+		this.setState({
+		  city: e.target.value
+		});
+	};
 	render(){
 		return(
 			<div>
-				<h1>I'm Working!</h1>
+				<h1>Weather Forecaster</h1>
+				<form onSubmit={this.getWeather}>
+					<input 
+						placeholder={"City"} 
+						type="text"
+						value={this.state.city}
+						onChange={this.changeCity}
+					/>
+				</form>
 			</div>
-		)
+		);
 	}
 }
 
